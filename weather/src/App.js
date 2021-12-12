50ac12ebf74bd06479b231371760b5d93c79630c
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-12-12 16:29:45 +0300

    Animation icon

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,13 +5,16 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <h2 className="title"><i className="fa fa--cloud"></i>Погода</h2>
+        <h2 className="title"><i className="fa fa-cloud"></i>Погода</h2>
         <div className="search-form">
           <input type="text" placeholder="Выберите город"/>
           <button type="button">Поиск</button> 
         </div>
         <div className="main-container">
-          <h4>weather Live</h4>
+          <h4>Погода онлайн</h4>
+          <div className="weather-icon">
+            <i className="fa fa-sun"></i>
+          </div>
         </div>
       </div>
     </div>
