7295947a3dee8912955081b095ab3810b9bd916c
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-12-13 11:39:34 +0300

    Work with API

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import {getWeatherData} from './data/weather'
 
 function App() {
+  // Объявляем переменные состояния weatherdata,city,loading.
+  const [weatherdata, setWeatherData] = useState(null)
+  const [city, setCity] = useState('Moscow')
+  const [loading, setLoading] = useState(false)
+// получаем данные
+  const getData = async () => {
+    try {
+      const data = await getWeatherData(city)
+      setWeatherData(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div className="App">
       <div className="card">
         <h2 className="title"><i className="fa fa-cloud"></i>Погода</h2>
         <div className="search-form">
-          <input type="text" placeholder="Выберите город"/>
-          <button type="button">Поиск</button> 
+          <input type="text" onChange={(e) => setCity(e.target.value)} placeholder="Выберите город"/>
+          <button type="button" onClick={() => getData()}>Поиск</button> 
         </div>
+        {weatherdata !== null ?(
         <div className="main-container">
           <h4>Погода онлайн</h4>
           <div className="weather-icon">
-            <i className="fa fa-sun"></i>
+            <img src={`http://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`} alt="imgicon"/>
           </div>
-          <h3>Солнечно</h3>
+          <h3>{weatherdata.weather[0].main}</h3>
           <div className="temprature">
-            <h1>-4&deg;C</h1>
+            <h1>{Math.round(weatherdata.main.temp - 273.15)}&deg;C</h1>
           </div>
           <div className="location">
-            <h3><i className="fa fa-street-view"></i>Москва | Россия</h3>
+            <h3><i className="fa fa-street-view"></i>{weatherdata.name} | {weatherdata.sys.country}</h3>
           </div>
           <div className="temprature-range">
-            <h6>Мин: -4&deg;C || Макс: -2&deg;C || Влажность: 50% </h6> 
+            <h6>Мин: {Math.round(weatherdata.main.temp_min - 273.15)}&deg;C || Макс: {Math.round(weatherdata.main.temp_max - 273.15)} &deg;C || Влажность: {weatherdata.main.humidity}%</h6>
           </div>
         </div>
+        ) : null}
+
       </div>
     </div>
   );
