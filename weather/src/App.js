e03cbe8535ee3f78168912e3da0254077ace369f
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-12-13 10:14:25 +0300

    Live Weather

--- weather/src/App.js
+++ weather/src/App.js
@@ -15,6 +15,16 @@ function App() {
           <div className="weather-icon">
             <i className="fa fa-sun"></i>
           </div>
+          <h3>Солнечно</h3>
+          <div className="temprature">
+            <h1>-4&deg;C</h1>
+          </div>
+          <div className="location">
+            <h3><i className="fa fa-street-view"></i>Москва | Россия</h3>
+          </div>
+          <div className="temprature-range">
+            <h6>Мин: -4&deg;C || Макс: -2&deg;C || Влажность: 50% </h6> 
+          </div>
         </div>
       </div>
     </div>
